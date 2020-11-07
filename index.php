dd8be1d990544e92c79a407884c11d922dc254df
Wahid Wahyu Adji <adhejisjdw@gmail.com>
2020-11-07 14:17:18 +0700

    menambahkan list di file index.html

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 	
 	<h1>COBA GIT REMOTE NICH</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis nisi, incidunt blanditiis vero, reprehenderit sed! Quam numquam possimus veritatis, consequuntur soluta, unde iste sed asperiores. Asperiores dolor sunt culpa.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 
 </body>
 </html>
